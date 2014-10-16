76489156b15e6ea74d5f43ef021a7f3b21760938
tcnksm <nsd22843@gmail.com>
2014-10-17 00:01:33 +0900

    Add OPT argument

--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@
 # Environmental variables 
 XC_ARCH="$WERCKER_GOX_ARCH"  
 XC_OS="$WERCKER_GOX_OS"
+XC_LDFLAGS="$WERCKER_GOX_LDFLAGS"
 
 XC_ARCH=${XC_ARCH:-386 amd64 arm}
 XC_OS=${XC_OS:-darwin linux windows freebsd openbsd netbsd plan9}
@@ -10,6 +11,7 @@ XC_OS=${XC_OS:-darwin linux windows freebsd openbsd netbsd plan9}
 gox \
     -os="${XC_OS}" \
     -arch="${XC_ARCH}" \
+    -ldflags="${XC_LDFLAGS}"\
     -output "${WERCKER_OUTPUT_DIR}/pkg/{{.OS}}_{{.Arch}}/{{.Dir}}"
 
 ls -l ${WERCKER_OUTPUT_DIR}/pkg
